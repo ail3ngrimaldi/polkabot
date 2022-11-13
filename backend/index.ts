2a061d87cc9a60cab13dba07bb24fb1585d4ad26
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2022-11-13 17:52:07 -0300

    file ready to start

--- backend/index.ts
+++ backend/index.ts
@@ -5,9 +5,11 @@ import {
     RichReply
 } from "matrix-bot-sdk";
 
+//We connect to our homeserver and add the chatbot access token
 const homeserverUrl = "https://matrix-client.matrix.org";
 const accessToken = "syt_YWlsZW50ZXN0aW5n_LWkbxdepBHhYwUFkKpbK_4MjItq";
 
+//Create a storage file and set autojoin to any room the bot is invited
 const storage = new SimpleFsStorageProvider("bot-storage.json");
 const client = new MatrixClient(homeserverUrl, accessToken, storage);
 AutojoinRoomsMixin.setupOnClient(client);
@@ -18,20 +20,23 @@ client.on("room.message", handleCommand);
 // Now that everything is set up, start the bot. This will start the sync loop and run until killed.
 client.start().then(() => console.log("Bot started!"));
 
-// This is the command handler we registered a few lines up
+// This is the command handler we registered before
 async function handleCommand(roomId: string, event: any) {
-    // Don't handle unhelpful events (ones that aren't text messages, are redacted, or sent by us)
+    // Don't handle unhelpful events (ones that aren't text messages, are redacted, or sent by the bot himself)
     if (event['content']?.['msgtype'] !== 'm.text') return;
     if (event['sender'] === await client.getUserId()) return;
     
-    // Check to ensure that the `hi polky` command is being run
-    // const body = event['content']['body'];
-    // if (!body?.startsWith("Hola polky")) return;<
-
-//    const replyBody = "Hola, ¿sobre qué querés aprender hoy?";
+// Possibility, if wanted you can start the bot with a specific word, in this case is `hola polky`
+// const body = event['content']['body'];
+// if (!body?.startsWith("Hola polky")) return;<
+// And the reply:
+//    const replyBody = "Hola, ¿sobre qué quierés aprender hoy?";
 //    const reply = RichReply.createFor(roomId, event, replyBody, replyBody);
 //    reply["msgtype"] = "m.notice";
 //    client.sendMessage(roomId, reply);
 
+// Instead we are using a more simple method, you can start Polky with any word (always as text)
     await client.replyNotice(roomId, event, "¡Hola! Mi nombre es Polky y estoy para responder todas tus dudas ¿Cómo puedo ayudarte?");
 }
+
+
